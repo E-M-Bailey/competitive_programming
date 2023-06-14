ab36b868116d7964f6f83e4bc40247e5cf8e0a1f
Evan Bailey <embailey02@gmail.com>
2023-06-14 15:31:10 -0500

    Kattis planetaris

---
+++ kattis/solved/planetaris.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n, a;
+	cin >> n >> a;
+	vector<int> E(n);
+	for (int& e : E)
+		cin >> e;
+	sort(E.rbegin(), E.rend());
+	int ans = 0;
+	while (!E.empty() && E.back() < a)
+	{
+		a -= E.back() + 1;
+		ans++;
+		E.pop_back();
+	}
+	cout << ans;
+}
