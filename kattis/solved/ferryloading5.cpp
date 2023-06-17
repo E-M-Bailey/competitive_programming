b6a34ec6c2900fb0c2e8cb92d33fb439bff96fe6
Evan Bailey <embailey02@gmail.com>
2023-06-17 12:55:34 -0500

    Kattis ferryloading5

---
+++ kattis/solved/ferryloading5.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long double T;
+
+constexpr int DIV = 1 << 15;
+
+int main()
+{
+	int n;
+	while (cin >> n, n)
+	{
+
+		vector<T> A(n);
+		for (auto &a : A)
+			cin >> a;
+
+		T sum = accumulate(begin(A), end(A), T(0));
+		T unit = sum / DIV;
+
+		vector<int> B(n);
+		for (int i = 0; i < n; i++)
+			B[i] = (int)round(A[i] / unit);
+		int m = accumulate(begin(B), end(B), 0);
+
+		vector<int> P(m + 1, -2);
+		P[0] = -1;
+		for (int i = n - 1; i >= 0; i--)
+		{
+			int b = B[i];
+			if (b == 0)
+				continue;
+			//cerr << ' ' << b << endl;
+			for (int j = m; j >= b; j--)
+			{
+				if (P[j - b] >= -1)
+					P[j] = i;
+			}
+		}
+
+		int best = -1;
+		for (int s = 0; s <= m; s++)
+			if (P[s] >= -1 && abs(2 * s - m) < abs(2 * best - m))
+			{
+				//cerr << ' ' << best << ' ' << s << endl;
+				best = s;
+			}
+
+		vector<int> ans;
+		int p;
+		while ((p = P[best]) >= 0)
+		{
+			//cerr << best << endl;
+			ans.push_back(p);
+			best -= B[p];
+		}
+		for (int a : ans)
+			cout << a + 1 << ' ';
+		cout << '\n';
+	}
+}
