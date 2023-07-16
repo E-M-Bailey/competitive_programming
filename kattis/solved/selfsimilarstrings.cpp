f62fb0448ae577fb5b9ff38c9447ee973315f41b
Evan Bailey <embailey02@gmail.com>
2023-07-15 19:03:21 -0500

    Kattis selfsimilarstrings

---
+++ kattis/solved/selfsimilarstrings.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	unordered_map<string, int> F;
+	while (cin >> s)
+	{
+		int ans = 1;
+		for (; ans < (int)s.size(); ans++)
+		{
+			F.clear();
+			for (int i = ans; i <= (int)s.size(); i++)
+				F[s.substr(i - ans, ans)]++;
+			bool works = true;
+			for (auto const &[_, f] : F)
+				works &= f > 1;
+			if (!works)
+				break;
+		}
+		cout << --ans << '\n';
+	}
+}
