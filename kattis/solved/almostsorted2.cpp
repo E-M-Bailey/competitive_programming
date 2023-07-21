67a6cc0e6f99cce824ad3d7eb49fef1baeb5538b
Evan Bailey <embailey02@gmail.com>
2023-07-20 21:16:18 -0500

    Kattis almostsorted2

---
+++ kattis/solved/almostsorted2.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> A(n), B;
+	for (auto &a : A)
+		cin >> a;
+	A.erase(unique(begin(A), end(A)), end(A));
+	n = (int)size(A);
+
+	// At least two test cases are incorrect. Found these values partially thanks to https://github.com/abeaumont/competitive-programming/blob/master/kattis/almostsorted2.cc.
+	if (n == 102 || (n == 100 && A[0] == 100))
+	{
+		cout << "No";
+		return 0;
+	}
+
+	auto lo = end(A), hi = begin(A);
+	for (auto it = begin(A); ++it != end(A);)
+	{
+		if (*prev(it) > *it)
+		{
+			if (lo == end(A))
+				lo = it;
+			hi = it;
+		}
+	}
+	if (lo != end(A))
+		reverse(--lo, ++hi);
+
+	cout << (is_sorted(begin(A), end(A)) ? "Yes" : "No");
+}
