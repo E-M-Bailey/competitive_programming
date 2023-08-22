8bc968abdfbeb6ae4b3f0ecb62d999e5e8b13e1a
Evan Bailey <embailey02@gmail.com>
2023-08-22 13:12:05 -0500

    Kattis pebblepuzzle

---
+++ kattis/solved/pebblepuzzle.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	array<__uint128_t, 100> A, B, C;
+	string s;
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+		fill(begin(A), begin(A) + m, 0);
+		fill(begin(B), begin(B) + m, 0);
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				if (cin >> s, s[0] == 'B')
+					A[j] |= (__uint128_t)1 << i;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				if (cin >> s, s[0] == 'B')
+					B[j] |= (__uint128_t)1 << i;
+		sort(begin(B), begin(B) + m);
+		bool ans = false;
+		for (int i = 0; !ans && i < m; i++)
+		{
+			__uint128_t mask = A[i] ^ B[0];
+			for (int j = 0; j < m; j++)
+				C[j] = A[j] ^ mask;
+			sort(begin(C), begin(C) + m);
+			ans = equal(begin(B), begin(B) + m, begin(C));
+		}
+		cout << (ans ? "YES\n" : "NO\n");
+	}
+}
