7d5253082f27bd7ab4f291c39994e5e7b10871c5
Evan Bailey <embailey02@gmail.com>
2023-09-21 19:09:44 -0500

    Kattis permutationdescent

---
+++ kattis/solved/permutationdescent.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int MOD = 1001113;
+
+int main(int argc, const char* argv[])
+{
+	if (argc > 1)
+	{
+		ignore = freopen(argv[1], "r", stdin);
+		ignore = freopen(argv[2], "w", stdout);
+	}
+
+	vector<vector<int>> DP(101);
+	DP[1].resize(1, 1);
+	for (int N = 1; N < 100; N++)
+	{
+		DP[N + 1].resize(N + 1);
+		for (int v = 0; v < N; v++)
+		{
+			DP[N + 1][v] = (DP[N + 1][v] + DP[N][v] * (v + 1)) % MOD;
+			DP[N + 1][v + 1] = (DP[N + 1][v + 1] + DP[N][v] * (N - v)) % MOD;
+		}
+	}
+
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int K, N, v;
+		cin >> K >> N >> v;
+		cout << K << ' ' << DP[N][v] << '\n';
+	}
+}
