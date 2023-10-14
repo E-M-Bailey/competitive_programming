d054348138dec89082719f2d34bc1f0ddb571b45
Evan Bailey <embailey02@gmail.com>
2023-10-14 12:37:16 -0500

    Kattis barclassification

---
+++ kattis/solved/barclassification.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<vector<bool>> G(n, vector<bool>(n));
+	int total = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			char c;
+			cin >> c;
+			if (c == '1')
+			{
+				G[i][j] = true;
+				total++;
+			}
+		}
+	}
+
+	bool row = false, col = false;
+	for (int i = 0; i < n; i++)
+	{
+		int ones = 0;
+		for (int j = 0; j < n; j++)
+			ones += G[i][j];
+		row |= total <= 2 * ones;
+	}
+	for (int j = 0; j < n; j++)
+	{
+		int ones = 0;
+		for (int i = 0; i < n; i++)
+			ones += G[i][j];
+		col |= total <= 2 * ones;
+	}
+
+	if (row && col)
+		cout << '+';
+	else if (row)
+		cout << '-';
+	else
+		cout << '|';
+}
