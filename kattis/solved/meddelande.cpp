f43c023cbc778be9cddcaaf30e9b6ffe40789123
Evan Bailey <embailey02@gmail.com>
2024-02-18 21:58:10 -0600

    kattis meddelande

---
+++ kattis/solved/meddelande.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int n, m;
+	cin >> n >> m;
+	char c;
+	while (cin >> c)
+		if (c != '.')
+			cout << c;
+}
