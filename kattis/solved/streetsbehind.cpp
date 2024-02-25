c4e48f13d6845f682d93548bb1db0e8109944e1f
Evan Bailey <embailey02@gmail.com>
2024-02-25 10:40:23 -0600

    kattis streetsbehind

---
+++ kattis/solved/streetsbehind.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		int64_t x, y, a, b;
+		cin >> x >> y >> a >> b;
+		int64_t c = b - a, ans = 0;
+		while (y)
+		{
+			int64_t d = c * x / a;
+			if (c * d == 0)
+			{
+				ans = -1;
+				break;
+			}
+			int64_t k = min(((d + 1) * a - c * x - 1) / (c * d) + 1, (y - 1) / d + 1), ch = min(k * d, y);
+			ans += k;
+			x += ch;
+			y -= ch;
+		}
+		cout << ans << '\n';
+	}
+}
