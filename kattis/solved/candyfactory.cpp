c3ddb5e7ce4f36ab1118cc6d947e447349db7c5c
Evan Bailey <embailey02@gmail.com>
2024-03-02 00:38:40 -0600

    kattis candyfactory

--- kattis/solved/candyfactory.cpp
+++ kattis/solved/candyfactory.cpp
@@ -4,17 +4,10 @@ using namespace std;
 
 int main()
 {
-	int n;
-	int64_t k, sum = 0, mx = 0;
+	int64_t n, k;
 	cin >> n >> k;
-
-	for (int i = 0; i < n; i++)
-	{
-		int64_t a;
-		cin >> a;
-		sum += a;
-		mx = max(mx, a);
-	}
-
-	cout << max(mx, (sum + k - 1) / k) * k - sum;
+	vector<int64_t> A(n);
+	copy(istream_iterator<int>(cin), istream_iterator<int>(), begin(A));
+	int64_t s = accumulate(begin(A), end(A), k - 1);
+	cout << max(*max_element(begin(A), end(A)), s / k) * k - s;
 }
