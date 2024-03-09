0a78ccea0db6bb8cba290b066751b43ffeee6ff0
Evan Bailey <embailey02@gmail.com>
2024-03-09 07:48:43 -0600

    kattis laviner

--- kattis/solved/laviner.cpp
+++ kattis/solved/laviner.cpp
@@ -1,39 +1,23 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 int main()
 {
 	cin.tie(0)->sync_with_stdio(0);
 	int n, k;
 	cin >> n >> k;
-
-	vector<int> P(n);
+	int P[n], S[n], l = 0, h = n + 1;
 	for (int i = 1; i < n; i++)
-	{
 		cin >> P[i];
-		P[i]--;
-	}
-
-	vector<int> S(n);
-
-	int lo = 0, hi = n;
-	while (lo < hi)
+	while (h - l > 1)
 	{
-		int mid	 = (lo + hi) / 2;
-		int cost = 0;
-		fill(begin(S), end(S), 1);
-		for (int i = n; i-- > 0;)
-		{
-			if (S[i] > mid)
-				cost++;
+		int m = (l + h - 1) / 2, c = 0;
+		fill_n(S, n, 1);
+		for (int i = n; i--;)
+			if (S[i] > m)
+				c++;
 			else
-				S[P[i]] += S[i];
-		}
-		if (cost > k)
-			lo = mid + 1;
-		else
-			hi = mid;
+				S[P[i] - 1] += S[i];
+		(c > k ? l : h) = m + 1;
 	}
-	cout << lo;
+	cout << l;
 }
