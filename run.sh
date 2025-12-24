a23b55da216040763c41d36c02d7e1c0faeab643
Evan Bailey <embailey02@gmail.com>
2025-12-24 13:53:43 -0600

    Fixed run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-test -f cp.marker || { >&2 printf -- 'Please call run from the repo root directory.' ; exit 1 ; }
+test -f cp.marker || { >&2 printf -- 'Please call run.sh from the repo root directory.' ; exit 1 ; }
 
 # TODO build for unconfigured judges
 
 case "$1" in
   j/ka/*|./j/ka/*)
     file="${1##*j/ka/}"
+    mkdir -p build/j/ka
     case "$1" in
       *.cpp)
-        base="${2%.cpp}"
+        base="${file%.cpp}"
         g++-14 -g -O2 -o build/j/ka/"$base" -std=gnu++23 -static -lrt -Wl,--whole-archive -lpthread -Wl,--no-whole-archive "$1"
         build/j/ka/"$base"
         ;;
