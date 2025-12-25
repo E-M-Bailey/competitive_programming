c0a9121b0028d6196778c57735317b50fec28e9f
Evan Bailey <embailey02@gmail.com>
2025-12-25 15:03:48 -0600

    Solved kattis rationalsequence2

---
+++ j/ka/rationalsequence2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int k;
+		string s;
+		cin >> k >> s;
+		auto pos = s.find('/');
+		int p = stoi(s.substr(0, pos)), q = stoi(s.substr(pos + 1));
+		
+		int ans = 0, p2 = 1;
+		while (p)
+		{
+			if (p < q)
+				q -= p;
+			else
+			{
+				p -= q;
+				ans += p2;
+			}
+			p2 *= 2;
+		}
+		
+		cout << k << ' ' << ans << '\n';
+	}
+}
