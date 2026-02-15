01059e3a59bc63fab52438e08709e2fc417d3394
Evan Bailey <embailey02@gmail.com>
2026-02-15 11:30:49 -0600

    Rename cp.marker to .cp.marker

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-test -f cp.marker || { >&2 printf -- 'Please call run.sh from the repo root directory.' ; exit 1 ; }
+test -f .cp.marker || { >&2 printf -- 'Please call run.sh from the repo root directory.' ; exit 1 ; }
 
 # TODO build for unconfigured judges
 
@@ -11,8 +11,21 @@ case "$1" in
     case "$1" in
       *.cpp)
         base="${file%.cpp}"
+        mkdir -p "$(dirname -- build/j/ka/"$base")"
         g++-14 -g -O2 -o build/j/ka/"$base" -std=gnu++23 -static -lrt -Wl,--whole-archive -lpthread -Wl,--no-whole-archive "$1" || exit
         build/j/ka/"$base"
+        ;;
+    esac
+    ;;
+  j/co/*|./j/co/*)
+    file="${1##*j/co/}"
+    mkdir -p build/j/co
+    case "$1" in
+      *.cpp)
+        base="${file%.cpp}"
+        mkdir -p "$(dirname -- build/j/co/"$base")"
+        g++-14 -Wall -Wextra -Wconversion -static -DONLINE_JUDGE -O2 -std=c++23 "$1" -lstdc++exp -o build/j/co/"$base" || exit
+        build/j/co/"$base"
         ;;
     esac
     ;;
