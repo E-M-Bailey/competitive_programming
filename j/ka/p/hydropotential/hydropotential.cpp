eb445bbe4c503a64830eb7984a5aeb4253a64b88
Evan Bailey <embailey02@gmail.com>
2026-02-24 20:23:22 -0600

    Solved kattis hydropotential

---
+++ j/ka/p/hydropotential/hydropotential.cpp
@@ -0,0 +1,14 @@
+#include "template.hpp"
+
+using namespace std;
+using namespace abbrev;
+
+int main()
+{
+	for (int n = 3; n--;)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << a * b << "0\n";
+	}
+}
